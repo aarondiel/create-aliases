ec2525853b88f0a4c31eb14a81bde1ccea43db65
aarondiel <aaron.diel@t-online.de>
2022-07-20 14:23:06 +0200

    feat(adding typescript compiler)

---
+++ dest/create_path_aliases.d.ts
@@ -0,0 +1,6 @@
+import type { AliasOptions } from "vite";
+import type { FileImporter } from "sass";
+export declare function parse_tsconfig(path?: string): [Record<string, string>, string];
+export declare function scss_aliases(aliases: Record<string, string>, directory_root: string): FileImporter<"sync">;
+export declare function vite_aliases(aliases: Record<string, string>, directory_root: string): AliasOptions;
+//# sourceMappingURL=create_path_aliases.d.ts.map
